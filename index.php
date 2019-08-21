260a24a6bfd125c595a4f088074e745fdda933f3
aysegulhatap <53220314+aysegulhatap@users.noreply.github.com>
2019-08-21 14:53:04 +0300

    kurumsal_web_demo
    kurumsalweb_demo

---
+++ index.php
@@ -0,0 +1,505 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	
+	<link rel="stylesheet" href="css/lightbox.min.css">
+	<link href="fontawesome/css/all.css" rel="stylesheet">
+	<!-- Style CSS -->
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+
+<body>
+
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+		<div class="container mt-5">
+			<button class="navbar-toggler" data-toggle="collapse" data-target="#nvbCollapse">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<a href="#" class="navbar-brand"><img src="" class="logo"></a>
+			<div class="collapse navbar-collapse" id="nvbCollapse">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#headerSection">
+						SHOPPING TIME</a>
+
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#features">ABOUT US</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#caseStudy">OUR WORKS</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#contact">CONTACT</a>
+					</li>
+					
+				</ul>
+
+
+			</div>
+		</div>
+	</nav>
+
+
+	<!--HEADER SECTİON-->
+
+	<header id="headerSection">
+		<div class="overlay">
+			<div class="container">
+				<div class="row text-center">
+					<div class="col my-auto">
+						<h1 class="display 3"><span></span></h1>
+						<p class="text-muted"><em>Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.</em>
+						</p>
+						<button type="button" class="btn btn-outline-primary">Action</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</header>
+
+	<!--FEATURES SECTİON-->
+
+	<section id="features" class="pt-5">
+		<div class="container">
+			<div class="row mt-5">
+				<div class="col-md-3 text-center">
+					<div class="feature">
+						<div class="features-child">
+							<i class="fab fa-ravelry"></i>
+							<h3 class="mt-3">?</h3>
+						</div>
+						<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.  </p>
+					</div>
+				</div>
+
+				<div class="col-md-3 text-center ">
+					<div class="feature">
+						<div class="features-child">
+							<i class="fas fa-cog"></i>
+							<h3 class="mt-3">?</h3>
+						</div>
+						<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.  </p>
+					</div>
+				</div>
+
+				<div class="col-md-3 text-center">
+					<div class="feature">
+						<div class="features-child">
+							<i class="fas fa-leaf"></i>
+							<h3 class="mt-3">?</h3>
+						</div>
+						<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.  </p>
+					</div>
+				</div>
+
+				<div class="col-md-3 text-center">
+					<div class="feature">
+						<div class="features-child">
+							<i class="fas fa-podcast"></i>
+							<h3 class="mt-3">?</h3>
+						</div>
+						<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<!--HİSTORY SECTİON-->
+	<section id="ourHistory">
+		<div class="container">
+			<div class="row mt-5">
+				<div class="col-md-6">
+					<img src="images/ayakkabı.jpg" class="img-fluid" alt="Our History" class="img-fluid bottompicture">
+				</div>
+				<div class="col-md-6 history">
+					<h2 class="mb-5">?</h2>
+					<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır</p>
+
+					<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır</p>
+
+					<button type="button" class="btn btn-outline-primary">?</button>
+				</div>
+			</div>
+		</div>
+
+	</section>
+
+	<!-- TEAM SECTİON -->
+
+	<section id="team" class="mt-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="card text-center">
+						<div class="card-block">
+							<img src="images/card-3.jpg" class="img-fluid rounded-circle my-4 ceo">
+							<h3>SANDALET</h3>
+							<h5 class="text-muted"></h5>
+							<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.</p>
+							<div class="d-flex flex-row justify-content-center">
+								<div class="p-2">
+									<a href="#"><i class="fab fa-facebook"></i></a>
+								</div>
+								<div class="p-2"><a href="#"><i class="fab fa-twitter"></i></a>
+								</div>
+								<div class="p-2">
+									<a href="#"><i class="fab fa-linkedin"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3">
+					<div class="card text-center">
+						<div class="card-block">
+							<img src="images/card-4.jpg" class="img-fluid rounded-circle my-4 ceo">
+							<h3>SANDALET</h3>
+							<h5 class="text-muted"></h5>
+							<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.</p>
+							<div class="d-flex flex-row justify-content-center">
+								<div class="p-2">
+									<a href="#"><i class="fab fa-facebook"></i></a></div>
+									<div class="p-2"><a href="#"><i class="fab fa-twitter"></i></a>
+									</div>
+									<div class="p-2">
+										<a href="#"><i class="fab fa-linkedin"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="card text-center">
+							<div class="card-block">
+								<img src="images/card-1.jpg" class="img-fluid rounded-circle my-4 ceo">
+								<h3>SANDALET</h3>
+								<h5 class="text-muted"></h5>
+								<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.</p>
+								<div class="d-flex flex-row justify-content-center">
+									<div class="p-2">
+										<a href="#"><i class="fab fa-facebook"></i></a>
+									</div>
+									<div class="p-2"><a href="#"><i class="fab fa-twitter"></i></a>
+									</div>
+									<div class="p-2">
+										<a href="#"><i class="fab fa-linkedin"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="card text-center">
+							<div class="card-block">
+								<img src="images/card-2.jpg" class="img-fluid rounded-circle my-4 ceo">
+								<h3>SANDALET</h3>
+								<h5 class="text-muted"></h5>
+								<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.</p>
+								<div class="d-flex flex-row justify-content-center">
+									<div class="p-2">
+										<a href="#"><i class="fab fa-facebook"></i></a></div>
+										<div class="p-2"><a href="#"><i class="fab fa-twitter"></i></a></div>
+										<div class="p-2">
+											<a href="#"><i class="fab fa-linkedin"></i></a></div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</section>
+
+				<!-- slider section -->
+
+				<section id="whatWeDo" class="py-5 px-5">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12 text-center mt-5">
+								<h2>?</h2>
+								<p class="text-muted"><em>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.</em></p>
+							</div>
+							<div class="col-md-6 text-center mx-auto">
+								<a href="#" class="video" data-video=""data-toggle="modal" data-target="#videoModal">
+									<i class="fas fa-play-circle"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</section>
+
+
+				<!-- slider section modalll Yapımı--> 
+				<div id="videoModal" class="modal fade" >
+					<div class="modal-dialog modal-lg">
+						<div class="modal-content">
+
+							<div class="modal-body">
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+								<iframe width="100%" height="400px" src="https://www.youtube.com/embed/D2SCMf142co" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							</div>
+						</div>
+					</div>
+				</div> 
+
+				<!-- case section -->
+				<section id="caseStudy" class="py-5">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="caseArea text-center">
+									<i class="fas fa-podcast"></i>
+									<h3>Nereden Bulabilirim?</h3>
+									<p>Lorem Ipsum pasajlarının birçok çeşitlemesi vardır. Ancak bunların büyük bir çoğunluğu mizah katılarak veya rastgele sözcükler eklenerek değiştirilmişlerdir.Eğer bir Lorem Ipsum pasajı kullanacaksanız, metin aralarına utandırıcı sözcükler gizlenmediğinden emin olmanız gerekir. İnternet'teki tüm Lorem Ipsum üreteçleri önceden belirlenmiş metin bloklarını yineler. Bu da, bu üreteci İnternet üzerindeki gerçek Lorem Ipsum üreteci yapar.</p>
+									<button type="button" class="btn btn-outline-primary">Action</button>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="caseImage">
+									<img src="images/ayakkabı1.jpg" class="img-fluid bottompicture" >
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+
+				<section id="red" class="py-5">
+					<div class="container">
+						<div class="row">
+
+						</div>
+					</div>
+				</section>
+
+				<!-- PORTFOLIO SECTİON -->
+
+				<section id="portfolio">
+					<div class="container-fluid">
+						<div class="row ">
+
+							<div class="col-md-3">
+								<a href="images/portfolia4.jpeg" data-lightbox="image-1" data-title="WEB DESING">
+									<div class="imageContainer">
+										<img src="images/portfolia4.jpeg" class="img-fluid bottompicture">
+										<div class="imageOverlay">
+											<div class="imageText">
+												<i class="fas fa-search"></i>
+												<h4>TASARIM</h4>
+												<p><em></em></p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+
+
+							<div class="col-md-3">
+								<a href="images/portfolia3.jpg" data-lightbox="image-1" data-title="WEB DESING">
+									<div class="imageContainer">
+										<img src="images/portfolia3.jpg" class="img-fluid bottompicture">
+										<div class="imageOverlay">
+											<div class="imageText">
+												<i class="fas fa-search"></i>
+												<h4>TASARIM</h4>
+												<p><em></em></p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+
+							<div class="col-md-3">
+								<a href="images/portfolia.jpg" data-lightbox="image-1" data-title="WEB DESING">
+									<div class="imageContainer">
+										<img src="images/portfolia.jpg" class="img-fluid bottompicture">
+
+										<div class="imageOverlay">
+											<div class="imageText">
+												<i class="fas fa-search"></i>
+												<h4>TASARIM</h4>
+												<p><em></em></p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+
+							<div class="col-md-3">
+								<a href="images/contac-2.jpeg" data-lightbox="image-1" data-title="WEB DESING">
+									<div class="imageContainer">
+										<img src="images/contac-2.jpeg" class="img-fluid bottompicture">
+										<div class="imageOverlay">
+											<div class="imageText">
+												<i class="fas fa-search"></i>
+												<h4>TASARIM</h4>
+												<p><em></em></p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</section>
+
+
+				<section id="testimonials" class="text-center py-5">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-8 mx-auto">
+								<div class="quote"><i class="fas fa-quote-left"></i></div>
+
+								<div id="mySlider" class="carousel slide" data-ride="carousel">
+									<ol class="carousel-indicators">
+										<li data-target="#mySlider" data-slide-to="0" class="active"></li>
+										<li data-target="#mySlider" data-slide-to="1"></li>
+										<li data-target="#mySlider" data-slide-to="2"></li>
+									</ol>
+									<div class="carousel-inner">
+										<div class="carousel-item active">
+											<img src="images/man.png" class="image-fluid rounded-circle png">
+											<div class="carousel-caption">
+												<blockquote class="blockquote">
+													<p class="mb-0"> Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.</p>
+													
+													<footer class="blockquote-footer">
+														<cite title="Job"></cite>
+													</footer>
+												</blockquote>
+											</div>
+										</div>
+
+										<div class="carousel-item">
+											<img src="images/man.png" class="image-fluid rounded-circle png">
+											<div class="carousel-caption">
+												<blockquote class="blockquote">
+													<p class="mb-0"> Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.</p>
+													
+													<footer class="blockquote-footer">
+														<cite title="Job"></cite>
+													</footer>
+												</blockquote>
+											</div>
+										</div>
+
+										<div class="carousel-item">
+											<img src="images/man.png" class="image-fluid rounded-circle png">
+											<div class="carousel-caption">
+												<blockquote class="blockquote">
+													<p class="mb-0"> Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.</p>
+													
+													<footer class="blockquote-footer">
+													<cite title="Job"></cite>
+													</footer>
+												</blockquote>
+											</div>
+										</div>
+									</div>
+
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+
+				<!-- CONTACT SECTİON --->
+
+				<section id="contact" class="py-5">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-7">
+								<h3>İLETİŞİM</h3>
+								<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır. </p>
+								<h4>Adress</h4>
+								<p> </p>
+								<h4>E-mail</h4>
+								<p></p>
+								<h4>Telefon</h4>
+								<p>555 444 333</p>
+								<div class="d-flex flex-row">
+									<div class="p-2">
+										<a href="#"><i class="fab fa-facebook"></i></a>
+									</div>
+									<div class="p-2">
+										<a href="#"><i class="fab fa-twitter"></i></a>
+									</div>
+
+									<div class="p-2">
+										<a href="#"><i class="fab fa-instagram"></i></a>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-lg-5">
+								<form>
+									<div class="input-group mb-3">
+										<div class="input-group-prepend">
+											<span class="input-group-text"><i class="fas fa-user"></i></span>
+										</div>
+										<input type="text" class="form-control">
+									</div>
+									<div class="input-group mb-3">
+										<div class="input-group-prepend">
+											<span class="input-group-text"><i class="fas fa-envelope"></i></span>
+										</div>
+										<input type="text" class="form-control">
+									</div>
+
+
+									<div class="input-group mb-3">
+										<div class="input-group-prepend">
+											<span class="input-group-text"><i class="fas fa-pencil-alt"></i></span>
+										</div>
+										<textarea class="form-control" placeholder="message" rows="5"></textarea>
+									</div>
+									<button type="submit" value="submit" class="btn btn-primary btn-block">GÖNDER</button>
+
+								</form>
+							</div>
+						</div>
+					</div>
+				</section>
+
+
+
+				<!-- FOOTER SECTİON --->
+
+				<footer id="main-footer" class="py-2">
+					<div class="container-fluid">
+						<div class="row text-center">
+							<div class="col-md-6 mx-auto">
+								<p class="lead">No Copyright &copy; 2017 BootMe</p>
+							</div>
+						</div>
+					</div> 
+				</footer>
+				<!-- Optional JavaScript -->
+				<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+				<script src="js/jquery-3.4.1.min.js"></script>
+				<script src="js/bootstrap.js"></script>
+
+				<script src="js/popper.min.js"></script>
+				<script src="js/main.js"></script>
+				<script src="js/lightbox.min.js"></script>
+			</body>
+			</html>
